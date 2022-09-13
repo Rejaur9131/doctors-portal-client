ffefaebc165150cd9730df0c6d0e8fa845ca566d
MD REJAUR RAHMAN <you@example.com>
2022-09-13 23:27:26 +0600

    assets and pages are added

---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='App'>
+      <h2 className='text-2xl font-medium'>This is about page</h2>
+    </div>
+  );
+};
+
+export default About;
